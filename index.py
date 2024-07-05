63103a55a7cc28faef79f757c77c0a71dfa5dd54
alissonfrazao <alissonseupreto@gmail.com>
2024-07-05 16:04:44 -0300

    inicio

---
+++ index.py
@@ -0,0 +1,15 @@
+print ('Hello world') 
+# -*- coding: encoding-*- 'spam eggs'  # single quotes
+
+"Paris rabbit got your back :)! Yay!"  # double quotes
+
+'1975'  # digits and numerals enclosed in quotes are also strings
+
+s = 'First line.\nSecond line.'  # \n means newline
+s  # without print(), special characters are included in the string
+'First line.\nSecond line.'
+print(s)  # with print(), special characters are interpreted, so \n produces new line
+First line.
+Second line.
+ 
+follo
