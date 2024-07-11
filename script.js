7ddc2089d2fd858de677e9784e46be920c45c71d
alissonfrazao <alissonseupreto@gmail.com>
2024-07-11 08:41:52 -0300

    alterações da nav bar

---
+++ script.js
@@ -0,0 +1,19 @@
+<script> 
+    function authenticateUser (username, password) {
+var accounts = apiService.sql ( 
+    "SELECT * FROM users"
+
+) ; 
+
+for (var i = 0; i Acoounts. length; i ++) { 
+Var account = accounts [i] ] 
+if  (account.username === username && 
+    {
+    
+}
+
+{
+
+
+    }        
+    }
